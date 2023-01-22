828e41ddb9a07820240f83e5e1416e08c3dd33c5
alphaseinor <alphaseinor@gmail.com>
2023-01-22 14:18:59 -0600

    added code from video and updated readme with change

--- server/README.md
+++ server/README.md
@@ -8,7 +8,10 @@ https://youtu.be/PEcJxkylcRM?list=PLillGF-RfqbYZty73_PHBqKRDnv7ikh68
 /server/customerbase
 
 ### Errors / Changes
+#### Part 2 
 1/22/23 No need to enable built in terminal, VSCode already has it
 1/22/23 Added PORT from process.env. Allows PORT to not be static based on environment
 1/22/23 export default expressGraphQL is now graphqlHTTP which is a non default export
 1/22/23 No error at 9:50 on part 2 video. 
+#### Part 3
+1/22/23 replaced for loop with customers.filter which returns first element of array
--- server/customerbase/schema.js
+++ server/customerbase/schema.js
@@ -7,6 +7,58 @@ const {
     GraphQLNonNull
 } = require("graphql")
 
+const customers = [
+    {
+        id: "1",
+        name: "Jane Doe",
+        email: "Jane@doe.com",
+        age: 35
+    },
+    {
+        id: "2",
+        name: "Joe Schmoe",
+        email: "Joe@Schmoe.com",
+        age: 35
+    },
+    {
+        id: "3",
+        name: "Brian Hague",
+        email: "brian@dontemailme.com",
+        age: 35
+    },
+]
+
+const CustomerType = new GraphQLObjectType({
+    name: "Customer",
+    fields: () => ({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        email: {type: GraphQLString},
+        age: {type: GraphQLInt}
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        customer: {
+            type: CustomerType,
+            args: {
+                id: {type: GraphQLString}
+            },
+            resolve(parentValue, args){
+                return customers.filter((customer) => customer.id === args.id)[0]
+            }
+        },
+        customers: {
+            type: new GraphQLList(CustomerType),
+            resolve(parentValue, args){
+                return customers
+            }
+        }
+    }
+})
+
 module.exports = new GraphQLSchema({
-    
+    query: RootQuery
 })
